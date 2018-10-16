7cbc0d621484c359809fadf0bda870ec6cb5f5d3
ravi <ravi096@gmail.com>
2018-10-16 00:51:59 -0700

    Added support for double digit addtions

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
-def _get_simple_additions(cnt):
+def _get_singledigit_additions(cnt):
     # Returns 9 entries
     _r = list(range(1, 10))
     n = (cnt + 8) / 9
@@ -17,9 +17,40 @@ def _get_simple_additions(cnt):
         _adds = _adds + zip(random.sample(_r, 9), random.sample(_r, 9)) 
     return _adds
 
-@app.route('/math')
-def add():
-    return render_template("addition.html", additions=_get_simple_additions(100))
+def _get_doubledigit_additions(cnt):
+    # Returns 9 entries
+    _r = list(range(10, 100))
+    n = (cnt + 8) / 9
+    _adds = []
+    for _i in range(n):
+        _adds = _adds + zip(random.sample(_r, 9), random.sample(_r, 9)) 
+    return _adds
+
+def generate_numbers1(st, end):
+    cnt = 100
+    _r = list(range(st, end))
+    n = (cnt + len(_r) - 1) / len(_r)
+    _adds = []
+    for _i in range(n):
+        _adds = _adds + zip(random.sample(_r, len(_r)), random.sample(_r, len(_r))) 
+    return _adds
+
+def generate_numbers(st, end, cnt=126):
+    _cnt = cnt
+    _adds = []
+    _r = list(range(st, end))
+    while _cnt > 0:
+        _adds += zip(random.sample(_r, len(_r)), random.sample(_r, len(_r)))
+        _cnt = _cnt - len(_r)
+    return _adds[:cnt]
+
+@app.route('/singledigit')
+def singledigit():
+    return render_template("addition.html", title="Single digit additions", additions=generate_numbers(1, 10))
+
+@app.route('/doubledigit')
+def doubledigit():
+    return render_template("addition.html", title="Double digit additions", additions=generate_numbers(10, 100))
 
 if __name__ == '__main__':
     app.run(debug=True)
