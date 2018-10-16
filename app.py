2e72c597578c277e1cc8e180b00435c9fd4b9c98
ravi <ravi096@gmail.com>
2018-10-16 01:06:49 -0700

    Added support for subtractions

--- app.py
+++ app.py
@@ -8,22 +8,37 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
-def generate_numbers(st, end, cnt=126):
+def generate_numbers(st, end, operator='+', cnt=126):
     _cnt = cnt
     _adds = []
     _r = list(range(st, end))
-    while _cnt > 0:
-        _adds += zip(random.sample(_r, len(_r)), random.sample(_r, len(_r)))
-        _cnt = _cnt - len(_r)
+    while len(_adds) < cnt:
+        for ii in zip(random.sample(_r, len(_r)), random.sample(_r, len(_r))):
+            if operator == '-':
+                if ii[0] <= ii[1]:
+                    continue
+            _adds.append(ii)
+        # _adds += zip(random.sample(_r, len(_r)), random.sample(_r, len(_r)))
+        # _cnt = _cnt - len(_r)
     return _adds[:cnt]
 
-@app.route('/singledigit')
-def singledigit():
-    return render_template("addition.html", title="Single digit additions", additions=generate_numbers(1, 10))
+@app.route('/singledigitaddition')
+def singledigitaddition():
+    return render_template("addition.html", operator='+', title="Single digit additions", additions=generate_numbers(1, 10))
 
-@app.route('/doubledigit')
-def doubledigit():
-    return render_template("addition.html", title="Double digit additions", additions=generate_numbers(10, 100))
+@app.route('/doubledigitaddition')
+def doubledigitaddition():
+    return render_template("addition.html", operator='+', title="Double digit additions", additions=generate_numbers(10, 100))
+
+@app.route('/singledigitsubtraction')
+def singledigitsubtraction():
+    return render_template("addition.html", operator='-', title="Single digit subtractions", 
+                            additions=generate_numbers(1, 10, operator='-'))
+
+@app.route('/doubledigitsubtraction')
+def doubledigitsubtraction():
+    return render_template("addition.html", operator='-', title="Double digit subtractions", 
+                            additions=generate_numbers(10, 100, operator='-'))
 
 if __name__ == '__main__':
     app.run(debug=True)
