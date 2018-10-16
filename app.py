5e5308d7ca843f658cb4d1c1b2f8c34cf43a1d72
ravi <ravi096@gmail.com>
2018-10-16 00:54:05 -0700

    Removed the old generator functions

--- app.py
+++ app.py
@@ -8,33 +8,6 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
-def _get_singledigit_additions(cnt):
-    # Returns 9 entries
-    _r = list(range(1, 10))
-    n = (cnt + 8) / 9
-    _adds = []
-    for _i in range(n):
-        _adds = _adds + zip(random.sample(_r, 9), random.sample(_r, 9)) 
-    return _adds
-
-def _get_doubledigit_additions(cnt):
-    # Returns 9 entries
-    _r = list(range(10, 100))
-    n = (cnt + 8) / 9
-    _adds = []
-    for _i in range(n):
-        _adds = _adds + zip(random.sample(_r, 9), random.sample(_r, 9)) 
-    return _adds
-
-def generate_numbers1(st, end):
-    cnt = 100
-    _r = list(range(st, end))
-    n = (cnt + len(_r) - 1) / len(_r)
-    _adds = []
-    for _i in range(n):
-        _adds = _adds + zip(random.sample(_r, len(_r)), random.sample(_r, len(_r))) 
-    return _adds
-
 def generate_numbers(st, end, cnt=126):
     _cnt = cnt
     _adds = []
