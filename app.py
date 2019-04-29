5a00e6c84a5f690f799fd67053ad4ca78e10d515
ravi <ravi096@gmail.com>
2019-04-29 00:25:47 -0700

    Added files for heroku hosting, Procfile and requirements.txt

--- app.py
+++ app.py
@@ -22,23 +22,50 @@ def generate_numbers(st, end, operator='+', cnt=126):
         # _cnt = _cnt - len(_r)
     return _adds[:cnt]
 
-@app.route('/singledigitaddition')
-def singledigitaddition():
+def generate_words():
+    with open('wordlist1.txt', 'r') as fh:
+        txt = fh.read()
+    for word in txt.split():
+        index = random.randint(0, len(word))
+        word[index] = '_'
+        print(word)
+    return None
+
+@app.route('/mul')
+def mul():
+    return render_template("addition.html", operator='x', title="Single digit multiplication", additions=generate_numbers(1, 10))
+
+@app.route('/bigmul')
+def bigmul():
+    return render_template("addition.html", operator='x', title="Double digit multiplication", additions=generate_numbers(10, 100))
+
+@app.route('/add')
+def add():
     return render_template("addition.html", operator='+', title="Single digit additions", additions=generate_numbers(1, 10))
 
-@app.route('/doubledigitaddition')
-def doubledigitaddition():
+@app.route('/bigadd')
+def bigadd():
     return render_template("addition.html", operator='+', title="Double digit additions", additions=generate_numbers(10, 100))
 
-@app.route('/singledigitsubtraction')
-def singledigitsubtraction():
-    return render_template("addition.html", operator='-', title="Single digit subtractions", 
+@app.route('/sub')
+def sub():
+    return render_template("addition.html", operator='-', title="Single digit subtractions",
                             additions=generate_numbers(1, 10, operator='-'))
 
-@app.route('/doubledigitsubtraction')
-def doubledigitsubtraction():
-    return render_template("addition.html", operator='-', title="Double digit subtractions", 
+@app.route('/bigsub')
+def bigsub():
+    return render_template("addition.html", operator='-', title="Double digit subtractions",
                             additions=generate_numbers(10, 100, operator='-'))
 
+@app.route('/words')
+def words():
+    # return render_template("words.html", words=generate_words())
+    generate_words()
+    return "OK"
+
+@app.route('/seq')
+def seq():
+    # return render_template("words.html", words=generate_words())
+    return render_template("seq.html")
 if __name__ == '__main__':
     app.run(debug=True)
